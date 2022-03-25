3d3b5495b378cbea801ded6da16afdcb38943094
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-25 20:31:14 -0300

    Executando no Chrome

--- CursoSelenium/src/main/java/TesteGoogle.java
+++ CursoSelenium/src/main/java/TesteGoogle.java
@@ -2,15 +2,27 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class TesteGoogle {
 
 	@Test
-	public void test() {
+	public void testFirefox() {
 		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https:www.google.com.br");
 		assertEquals("Google", driver.getTitle());
+		driver.close();
+	}
+	
+	@Test
+	public void testChrome() {
+		System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver\\99.0\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https:www.google.com.br");
+		assertEquals("Google", driver.getTitle());
+		driver.close();
 	}
 }
