5d4a88cff55096abd1e7842ca9dbbdb55126190d
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-25 22:07:55 -0300

    Elementos básicos: TextField e TextArea

--- CursoSelenium/src/main/java/TesteCampoTreinamento.java
+++ CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -1,4 +1,7 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -6,15 +9,28 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class TesteCampoTreinamento {
 
 	@Test
-	public void teste() {
+	public void testeTextField() {
 		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
-		driver.manage().window().setSize(new Dimension(800, 600));
+		driver.manage().window().setSize(new Dimension(900, 600));
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/site/componentes.html");
 		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Texto Um");
+		assertEquals("Texto Um", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));;
 		
+		driver.close();
+	}
+	
+	@Test
+	public void deveInteragirComTextArea() {
+		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(900, 600));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/site/componentes.html");
 		
-		
-		//driver.close();
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Test");
+		assertEquals("Test", driver.findElement(By.id("elementosForm:sobrenome")).getAttribute("value"));
+	
+		driver.close();
 	}
 }
