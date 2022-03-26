e50c59d7f00cb836c5d144ffb9f06409caaf5d98
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-26 18:27:05 -0300

    Elementos básicos: Combo

--- CursoSelenium/src/main/java/TesteCampoTreinamento.java
+++ CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -1,11 +1,15 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteCampoTreinamento {
 
@@ -58,6 +62,48 @@ public class TesteCampoTreinamento {
 		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
 		assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
 		
+		driver.close();
+	}
+	
+	@Test
+	public void deveInteragirComCombo() {
+		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(900, 600));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/site/componentes.html");
+		
+		WebElement elemento = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(elemento);
+		//combo.selectByIndex(1);
+		//combo.selectByValue("superior");
+		combo.selectByVisibleText("2o grau completo");
+		
+		assertEquals("2o grau completo", combo.getFirstSelectedOption().getText());
+		driver.close();
+	}
+	
+	@Test
+	public void deveVerificarValoresCombo() {
+		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(900, 600));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/site/componentes.html");
+		
+		WebElement elemento = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(elemento);
+		List<WebElement> options = combo.getOptions();
+		
+		assertEquals(8, options.size());
+		
+		boolean encontrou = false;
+		for(WebElement option: options) {
+			if(option.getText().equals("Mestrado")) {
+				encontrou = true;
+				break;
+			}
+		}
+		
+		assertTrue(encontrou);
 		driver.close();
 	}
 }
