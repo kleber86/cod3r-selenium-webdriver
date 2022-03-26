5c9c9c48912ecd2cf67e7b4ab3ebf01156a3139a
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-25 21:43:07 -0300

    Posição e tamanho do browser

--- CursoSelenium/src/main/java/TesteGoogle.java
+++ CursoSelenium/src/main/java/TesteGoogle.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,6 +14,7 @@ public class TesteGoogle {
 	public void testFirefox() {
 		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(800, 600));
 		driver.get("https:www.google.com.br");
 		assertEquals("Google", driver.getTitle());
 		driver.close();
@@ -21,6 +24,7 @@ public class TesteGoogle {
 	public void testChrome() {
 		System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver\\99.0\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https:www.google.com.br");
 		assertEquals("Google", driver.getTitle());
 		driver.close();
