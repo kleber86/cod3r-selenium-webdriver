e3ce6e4e0f8d74c42d1710f1a6d3a95718f6633b
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-30 20:31:12 -0300

    Frames

---
+++ CursoSelenium/src/main/java/TesteFrames.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteFrames {
+
+	@Test
+	public void devePreencherFrames() {
+		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1000, 800));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/site/componentes.html");
+	
+		driver.switchTo().frame("frame1");
+		driver.findElement(By.id("frameButton")).click();
+		
+		Alert alert = driver.switchTo().alert();
+		String msg = alert.getText();
+		assertEquals("Frame OK!", msg);
+		alert.accept();
+		
+		driver.switchTo().defaultContent();
+		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys(msg);
+		
+		driver.close();
+	}
+}
