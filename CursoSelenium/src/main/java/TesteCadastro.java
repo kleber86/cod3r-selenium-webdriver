73a609a4b383b302dfdcaf618ae827fe34121987
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-04-02 05:43:40 -0300

    ajustes

--- CursoSelenium/src/main/java/TesteCadastro.java
+++ CursoSelenium/src/main/java/TesteCadastro.java
@@ -4,17 +4,14 @@ import static org.junit.Assert.assertTrue;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class TesteCadastro {
 
 	private WebDriver driver;
-	private DSL dsl;
 	private CampoTreinamentoPage page;
 	
 	@Before
@@ -23,7 +20,6 @@ public class TesteCadastro {
 		driver = new FirefoxDriver();
 		driver.manage().window().setSize(new Dimension(900, 600));
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/site/componentes.html");
-		dsl = new DSL(driver);
 		page = new CampoTreinamentoPage(driver);
 	}
 	
