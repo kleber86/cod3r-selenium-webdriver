15f221de17d3177983ceaed81af5766001f8be91
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-04-03 10:47:25 -0300

    Desafio: Combo prime

--- CursoSelenium/src/main/java/TestePrime.java
+++ CursoSelenium/src/main/java/TestePrime.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
@@ -17,7 +18,7 @@ public class TestePrime {
 	public void inicializa() {
 		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
 		driver = new FirefoxDriver();
-		driver.manage().window().setSize(new Dimension(900, 600));
+		driver.manage().window().setSize(new Dimension(1200, 900));
 		driver.get("https://www.primefaces.org/showcase/ui/input/oneRadio.xhtml");
 		dsl = new DSL(driver);
 	}
@@ -34,8 +35,15 @@ public class TestePrime {
 		
 		dsl.clicarRadio(By.xpath("//label[.='Option2']/..//span"));
 		assertTrue(dsl.isRadioMarcado("j_idt312:console:1"));
+	}
+	
+	@Test
+	public void deveSelecionarCombro() throws InterruptedException {
+		driver.get("https://www.primefaces.org/showcase/ui/input/oneMenu.xhtml");
 		
-		
+		dsl.clicarRadio(By.xpath("//*[@id='j_idt311:option_label']/../div/span"));
+		dsl.clicarRadio(By.xpath("//*[@id='j_idt311:option_1']"));
+		System.out.println(":::::::::::::::::" + driver.findElement(By.xpath("//*[@id='j_idt311:option_input']/option[2]")).getText());
 	}
 	
 }
