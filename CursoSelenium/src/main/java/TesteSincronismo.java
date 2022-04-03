1a82c119a60a3973b630da593a545c808ef9484f
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-04-03 11:43:23 -0300

    Espera Fixa

--- CursoSelenium/src/main/java/TesteSincronismo.java
+++ CursoSelenium/src/main/java/TesteSincronismo.java
@@ -9,13 +9,14 @@ public class TesteSincronismo {
 
 	private WebDriver driver;
 	private DSL dsl;
+	private CampoTreinamentoPage page;
 	
 	@Before
 	public void inicializa() {
 		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().window().setSize(new Dimension(1200, 900));
-		driver.get("https://www.primefaces.org/showcase/ui/input/oneRadio.xhtml");
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/site/componentes.html");
 		dsl = new DSL(driver);
 	}
 	
@@ -24,10 +25,11 @@ public class TesteSincronismo {
 		driver.close();
 	}
 	
+	
 	@Test
-	public void deveIteragirComResportaDemorada() {
-		// Sem solução
+	public void deveIteragirComResportaDemorada() throws InterruptedException {
 		dsl.clicarBotao("buttonDelay");
+		Thread.sleep(5000);
 		dsl.escreve("novoCampo", "Deu certo?");
 	}
 }
