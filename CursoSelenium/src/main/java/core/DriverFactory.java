3d39fcc939d6399867e5421e6f3aedaf3b1c902e
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-04-04 22:15:48 -0300

    Chaveamento do browser

--- CursoSelenium/src/main/java/core/DriverFactory.java
+++ CursoSelenium/src/main/java/core/DriverFactory.java
@@ -1,6 +1,7 @@
 package core;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
@@ -10,9 +11,19 @@ private static WebDriver driver;
 	private DriverFactory() {}
 	
 	public static WebDriver getDriver(){
-		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
+		
 		if(driver == null) {
-			driver = new FirefoxDriver();
+			switch (Propriedades.browsers) {
+			case FIREFOX: 
+				System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");	
+				driver = new FirefoxDriver(); 
+				break;
+			case CHROME: 
+				System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver\\99.0\\chromedriver.exe");
+				driver = new ChromeDriver(); 
+				break;
+			}
+
 			driver.manage().window().maximize();		
 		}
 		return driver;
--- CursoSelenium/src/main/java/core/Propriedades.java
+++ CursoSelenium/src/main/java/core/Propriedades.java
@@ -3,4 +3,12 @@ package core;
 public class Propriedades {
 
 	public static boolean FECHAR_BROWSER = false;
+	
+	public static Browsers browsers = Browsers.CHROME;
+	
+	
+	public enum Browsers{
+		CHROME,
+		FIREFOX
+	}
 }
