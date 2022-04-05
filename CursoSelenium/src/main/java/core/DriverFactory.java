6277d19d169615c5c3c60da745bfe2835168e9cf
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-04-04 22:06:26 -0300

    Screenshot ao final dos testes

--- CursoSelenium/src/main/java/core/DriverFactory.java
+++ CursoSelenium/src/main/java/core/DriverFactory.java
@@ -1,6 +1,5 @@
 package core;
 
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -14,7 +13,7 @@ private static WebDriver driver;
 		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
 		if(driver == null) {
 			driver = new FirefoxDriver();
-			driver.manage().window().setSize(new Dimension(1200, 765));			
+			driver.manage().window().maximize();		
 		}
 		return driver;
 	}
