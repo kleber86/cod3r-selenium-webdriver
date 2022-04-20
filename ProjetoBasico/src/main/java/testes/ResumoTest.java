1fbe2717f39b6060c67e12ec76400d92480653c3
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-04-20 20:51:42 -0300

    Desafio: Teste de resumo mensal vazio

--- ProjetoBasico/src/main/java/testes/ResumoTest.java
+++ ProjetoBasico/src/main/java/testes/ResumoTest.java
@@ -3,11 +3,16 @@ package testes;
 import static core.DriverFactory.getDriver;
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import core.BaseTest;
+import core.DriverFactory;
 import pages.MenuPage;
 import pages.ResumoPage;
 
@@ -16,19 +21,24 @@ public class ResumoTest extends BaseTest {
 
 	private MenuPage menuPage = new MenuPage();
 	private ResumoPage resumoPage = new ResumoPage();
-	
+
 	@Test
 	public void test1RemoverMovimentacao() {
 		menuPage.acessarTelaResumo();
-		
+
 		resumoPage.excluirMovimentacao();
 		assertEquals("Movimentação removida com sucesso!", resumoPage.obterMensagemSucesso());
 	}
-	
+
 	@Test
 	public void test2ResumoMensal() {
 		menuPage.acessarTelaResumo();
-		
+
 		assertEquals("Seu Barriga - Extrato", getDriver().getTitle());
+
+		List<WebElement> elementosEncontrados =
+		DriverFactory.getDriver().findElements(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+
+		assertEquals(0, elementosEncontrados.size());
 	}
 }
