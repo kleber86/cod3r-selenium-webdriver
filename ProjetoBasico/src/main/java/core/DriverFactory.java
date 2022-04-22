147a0648e05bbaff831fac337fdbf2e5a28afdaf
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-04-21 22:53:40 -0300

    Refatorando os testes para uso do Selenium GRID

--- ProjetoBasico/src/main/java/core/DriverFactory.java
+++ ProjetoBasico/src/main/java/core/DriverFactory.java
@@ -1,8 +1,15 @@
 package core;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import core.Propriedades.TipoExecucao;
 
 public class DriverFactory {
 
@@ -21,22 +28,36 @@ public class DriverFactory {
 		return threadDriver.get();
 	}
 
-	public static WebDriver initDriver() {
+	public static WebDriver initDriver(){
 		WebDriver driver = null;
-		switch (Propriedades.browsers) {
-		case FIREFOX:
-			System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver\\0.30.0\\geckodriver.exe");
-			driver = new FirefoxDriver();
-			break;
-		case CHROME:
-			System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver\\99.0\\chromedriver.exe");
-			driver = new ChromeDriver();
-			break;
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.LOCAL) {
+			switch (Propriedades.BROWSER) {
+				case FIREFOX: 
+					driver = new FirefoxDriver(); break;
+				case CHROME: 
+					driver = new ChromeDriver(); 
+					break;
+			}
 		}
-
-		driver.manage().window().maximize();
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.GRID) {
+			DesiredCapabilities cap = null;
+			switch (Propriedades.BROWSER) {
+				case FIREFOX: 
+					cap = DesiredCapabilities.firefox(); break;
+				case CHROME: 
+					cap = DesiredCapabilities.chrome(); break;
+			}
+			try {
+				driver = new RemoteWebDriver(new URL(" http://192.168.56.1:4444/wd/hub"), cap);
+			} catch (MalformedURLException e) {
+				System.err.println("Falha na conexão com o GRID");
+				e.printStackTrace();
+			}
+		}
+		driver.manage().window().maximize();			
 		return driver;
 	}
+
 
 	public static void killDriver() {
 		WebDriver driver = getDriver();
--- ProjetoBasico/src/main/java/core/Propriedades.java
+++ ProjetoBasico/src/main/java/core/Propriedades.java
@@ -1,13 +1,20 @@
 package core;
 
 public class Propriedades {
-
+	
 	public static boolean FECHAR_BROWSER = true;
 	
-	public static Browsers browsers = Browsers.CHROME;
-		
-	public enum Browsers{
+	public static Browsers BROWSER = Browsers.CHROME;
+	
+	public static TipoExecucao TIPO_EXECUCAO = TipoExecucao.GRID;
+	
+	public enum Browsers {
 		CHROME,
 		FIREFOX
+	}
+	
+	public enum TipoExecucao {
+		LOCAL,
+		GRID
 	}
 }
